afbcc99ca3db967630f64dc9e1c03053b93718e2
Kasper Holm Hansen <khha.skp@edu.mercantec.dk>
2020-02-10 09:37:33 +0100

    Added some documentation to the hashmap project

--- KHHAHashMap/KHHAHashMap/HashMap.cs
+++ KHHAHashMap/KHHAHashMap/HashMap.cs
@@ -7,20 +7,52 @@ using System.Threading.Tasks;
 
 namespace KHHAHashMap
 {
+    /// <summary>
+    /// This is a custom implementation of the collection type HashTable.
+    /// A HashTable stores data in pairs that are retrievable at will with relatively fast access times.
+    /// 
+    /// At best O(1), at worst O(N).
+    /// </summary>
+    /// <typeparam name="KeyType"></typeparam>
+    /// <typeparam name="DataType"></typeparam>
     public class HashMap<KeyType, DataType> : IEnumerable
     {
+        /// <summary>
+        /// Indexer that can be used just like in any other native collection, as long as the types match
+        /// </summary>
+        /// <param name="key">Key used to select a bucket to retrieve data from. Type must match with <see cref="KeyType"/> supplied during creation</param>
+        /// <returns>Whatever is in the matching bucket to the key supplied. Can be null</returns>
         public DataType this[KeyType key]
         {
             get => Get(key);
             set => Add(key, value);
         }
 
+        /// <summary>
+        /// Amount of buckets used. As some hashes may collide, there may be more elements in the collection than indexes used. While the collection is still able retrieve the data, there will be a performance slow down proportional to the difference in indexes used and element count.
+        /// </summary>
         private int indexUsage;
+        /// <summary>
+        /// The amount of elements in the collection.
+        /// </summary>
         private int elementCount;
+        /// <summary>
+        /// The amount of buckets used to store elements
+        /// </summary>
         private int capacity;
+        /// <summary>
+        /// Array of buckets used to store elements. While capacity may seem like a hard-cap, it is not. Whenever there is a hash-collision, the collection uses a variation of a linked list to differentiate and store the said element.
+        /// </summary>
         private LinkedPair<KeyType, DataType>[] buckets;
+        /// <summary>
+        /// List of all keys in the collection. Novelty method also implemented in most other collections
+        /// </summary>
         private List<KeyType> keys;
 
+        /// <summary>
+        /// Creates a <see cref="HashMap{KeyType, DataType}"/> of the supplied types and with a set capacity.
+        /// </summary>
+        /// <param name="capacity">non-negative number specifying the bucket count of the collection.</param>
         public HashMap(int capacity)
         {
             this.capacity = capacity;
@@ -31,6 +63,11 @@ namespace KHHAHashMap
             keys = new List<KeyType>();
         }
 
+        /// <summary>
+        /// Calculates the bucket index of a key
+        /// </summary>
+        /// <param name="key">Key to calculate bucket index of</param>
+        /// <returns>Bucket index</returns>
         private uint CalculateIndex(KeyType key)
         {
             uint hash = (uint)key.GetHashCode();
@@ -38,6 +75,10 @@ namespace KHHAHashMap
             return index;
         }
 
+        /// <summary>
+        /// Resizes the <see cref="buckets"/> by a specified amount
+        /// </summary>
+        /// <param name="deltaCapacity">Amount to change the bucket array of. Can be negative if shrinking the bucket array is desired.</param>
         public void Resize(int deltaCapacity)
         {
             indexUsage = 0;
@@ -57,21 +98,38 @@ namespace KHHAHashMap
             }
         }
 
+        /// <summary>
+        /// Load factor is the amount of indexes used divided by the amount of indexes available (buckets)
+        /// </summary>
+        /// <returns>Usage percentage of indexes</returns>
         public float GetLoadFactor()
         {
             return indexUsage / (float)capacity;
         }
 
+        /// <summary>
+        /// Returns the amount of elements in the collection
+        /// </summary>
+        /// <returns>Element count</returns>
         public int Count()
         {
             return elementCount;
         }
 
+        /// <summary>
+        /// Returns keys of all elements in collection
+        /// </summary>
+        /// <returns>Readonly collection of keys</returns>
         public ICollection<KeyType> GetKeys()
         {
             return keys.AsReadOnly();
         }
 
+        /// <summary>
+        /// Adds a new element to the collection with a specified key
+        /// </summary>
+        /// <param name="key">Key of new element</param>
+        /// <param name="data">Element to add</param>
         public void Add(KeyType key, DataType data)
         {
             if(data == null)
@@ -91,14 +149,14 @@ namespace KHHAHashMap
             }
             else
             {
-                LinkedPair<KeyType, DataType> pair = buckets[index].GetLastLinkedPairInLink();
+                LinkedPair<KeyType, DataType> pair = buckets[index].GetLinkedPairWithKeyInList(key);
                 if(pair != null)
                 {
                     pair.SetData(data);
                 }
                 else
                 {
-                    pair = new LinkedPair<KeyType, DataType>(buckets[index], key, data);
+                    pair = new LinkedPair<KeyType, DataType>(key, data, buckets[index]); // Automatically sets next as the previous element in bucket
                     buckets[index].SetPrevious(pair);
                     buckets[index] = pair;
                     elementCount++;
@@ -106,6 +164,11 @@ namespace KHHAHashMap
             }
         }
 
+        /// <summary>
+        /// Gets element with a specified key. Returns null if no matched element is found
+        /// </summary>
+        /// <param name="key">Key of requested element</param>
+        /// <returns>Matched element if it exists, otherwise null</returns>
         public DataType Get(KeyType key)
         {
             uint index = CalculateIndex(key);
@@ -130,6 +193,11 @@ namespace KHHAHashMap
             }
         }
 
+        /// <summary>
+        /// Remove key and matching data from collection
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public bool Remove(KeyType key)
         {
             uint index = CalculateIndex(key);
@@ -152,7 +220,7 @@ namespace KHHAHashMap
                     previous = match.GetPrevious();
                     next = match.GetNext();
 
-                    if(previous == null && next == null)
+                    if(previous == null && next == null) // Matched pair is the only pair in the bucket
                     {
                         buckets[index] = null;
                         keys.Remove(key);
@@ -160,7 +228,7 @@ namespace KHHAHashMap
                         indexUsage--;
                         return true;
                     }
-                    else if(previous == null)
+                    else if(previous == null) // Matched pair is at the beginning of the bucket list
                     {
                         buckets[index] = next;
                         match.SetNext(null);
@@ -168,14 +236,14 @@ namespace KHHAHashMap
                         elementCount--;
                         return true;
                     }
-                    else if(next == null)
+                    else if(next == null) // Matched pair is at the end of the bucket list
                     {
                         match.SetPrevious(null);
                         previous.SetNext(null);
                         elementCount--;
                         return true;
                     }
-                    else
+                    else // Matched pair is in the middle of the bucket list
                     {
                         previous.SetNext(next);
                         next.SetPrevious(previous);
@@ -188,15 +256,33 @@ namespace KHHAHashMap
             }
         }
 
+        /// <summary>
+        /// Get enumerator of the collection. Part of the <see cref="IEnumerable"/> interface
+        /// </summary>
+        /// <returns>Enumerator of collection</returns>
         public IEnumerator GetEnumerator()
         {
             return new HashMapEnumerator(this);
         }
 
+        /// <summary>
+        /// Enumerator class for the collection.
+        /// 
+        /// This class's sole purpose is to iterate through the <see cref="HashMap{KeyType, DataType}"/> collection. It is self-contained and does not write anything to the <see cref="HashMap{KeyType, DataType}"/>.
+        /// </summary>
         private class HashMapEnumerator : IEnumerator
         {
+            /// <summary>
+            /// All buckets in supplied <see cref="HashMap{KeyType, DataType}"/>
+            /// </summary>
             private LinkedPair<KeyType, DataType>[] buckets;
+            /// <summary>
+            /// Index of the current bucket
+            /// </summary>
             private int bucketIndex;
+            /// <summary>
+            /// Enumerator of the current bucket (See <see cref="LinkedPair{KeyType, DataType}.LinkedPairEnumerator"/>)
+            /// </summary>
             private IEnumerator bucketEnumerator = null;
 
             public object Current { get => bucketEnumerator.Current; }
@@ -208,6 +294,9 @@ namespace KHHAHashMap
                 FindNextPopulatedBucket();
             }
 
+            /// <summary>
+            /// Finds the next bucket that contains elements after <see cref="bucketIndex"/>
+            /// </summary>
             private void FindNextPopulatedBucket()
             {
                 while(++bucketIndex < buckets.Length)
@@ -222,12 +311,19 @@ namespace KHHAHashMap
                 bucketEnumerator = null;
             }
 
+            /// <summary>
+            /// Part of the <see cref="IEnumerator"/> interface
+            /// </summary>
             public void Reset()
             {
                 bucketIndex = 0;
                 FindNextPopulatedBucket();
             }
 
+            /// <summary>
+            /// Part of the <see cref="IEnumerator"/> interface
+            /// </summary>
+            /// <returns>Next element in collection</returns>
             public bool MoveNext()
             {
                 if (bucketEnumerator.MoveNext())
--- KHHAHashMap/KHHAHashMap/LinkedPair.cs
+++ KHHAHashMap/KHHAHashMap/LinkedPair.cs
@@ -7,12 +7,20 @@ using System.Threading.Tasks;
 
 namespace KHHAHashMap
 {
+    /// <summary>
+    /// Key-Data pair with <see cref="LinkedList{T}"/>-like capabilities
+    /// </summary>
+    /// <typeparam name="KeyType"></typeparam>
+    /// <typeparam name="DataType"></typeparam>
     public class LinkedPair<KeyType, DataType> : IComparable, IEnumerable
     {
         private LinkedPair<KeyType, DataType> previous = null;
         private LinkedPair<KeyType, DataType> next = null;
         private KeyType key = default;
         private DataType data = default;
+        /// <summary>
+        /// Whether or not the <see cref="DataType"/> can be compared through the <see cref="IComparable"/> interface
+        /// </summary>
         private bool isDataComparable = false;
 
         public LinkedPair()
@@ -20,43 +28,48 @@ namespace KHHAHashMap
             DetermineDataComparable();
         }
 
-        public LinkedPair(KeyType key, DataType data)
+        public LinkedPair(KeyType key, DataType data) : this()
         {
             this.key = key;
             this.data = data;
-            DetermineDataComparable();
         }
 
-        public LinkedPair(KeyType key, DataType data, LinkedPair<KeyType, DataType> next)
+        public LinkedPair(KeyType key, DataType data, LinkedPair<KeyType, DataType> next) : this()
         {
             this.key = key;
             this.data = data;
             this.next = next;
-            DetermineDataComparable();
         }
 
-        public LinkedPair(LinkedPair<KeyType, DataType> previous, KeyType key, DataType data)
+        public LinkedPair(LinkedPair<KeyType, DataType> previous, KeyType key, DataType data) : this()
         {
             this.key = key;
             this.data = data;
             this.previous = previous;
-            DetermineDataComparable();
         }
 
-        public LinkedPair(KeyType key, DataType data, LinkedPair<KeyType, DataType> previous, LinkedPair<KeyType, DataType> next)
+        public LinkedPair(KeyType key, DataType data, LinkedPair<KeyType, DataType> previous, LinkedPair<KeyType, DataType> next) : this()
         {
             this.key = key;
             this.data = data;
             this.previous = previous;
             this.next = next;
-            DetermineDataComparable();
         }
 
+        /// <summary>
+        /// Get key of the <see cref="LinkedPair{KeyType, DataType}"/>
+        /// </summary>
+        /// <returns></returns>
         public KeyType GetKey()
         {
             return key;
         }
 
+        /// <summary>
+        /// Replace key and return the old one
+        /// </summary>
+        /// <param name="newKey">Replacement key</param>
+        /// <returns>Old key</returns>
         public KeyType ReplaceKey(KeyType newKey)
         {
             KeyType oldKey = key;
@@ -64,16 +77,29 @@ namespace KHHAHashMap
             return oldKey;
         }
 
+        /// <summary>
+        /// Replace the key
+        /// </summary>
+        /// <param name="newKey">New key</param>
         public void SetKey(KeyType newKey)
         {
             key = newKey;
         }
 
+        /// <summary>
+        /// Get the data of the <see cref="LinkedPair{KeyType, DataType}"/>
+        /// </summary>
+        /// <returns></returns>
         public DataType GetData()
         {
             return data;
         }
 
+        /// <summary>
+        /// Replace then return the old data
+        /// </summary>
+        /// <param name="newData">Replacement data</param>
+        /// <returns>Old data</returns>
         public DataType ReplaceData(DataType newData)
         {
             DataType oldData = data;
@@ -81,16 +107,29 @@ namespace KHHAHashMap
             return oldData;
         }
 
+        /// <summary>
+        /// Replace the data
+        /// </summary>
+        /// <param name="newData">New Data</param>
         public void SetData(DataType newData)
         {
             data = newData;
         }
 
+        /// <summary>
+        /// Get next linked <see cref="LinkedPair{KeyType, DataType}"/>. Can be null
+        /// </summary>
+        /// <returns></returns>
         public LinkedPair<KeyType, DataType> GetNext()
         {
             return next;
         }
 
+        /// <summary>
+        /// Replace next link
+        /// </summary>
+        /// <param name="newNext">New next pair</param>
+        /// <returns>Old next pair</returns>
         public LinkedPair<KeyType, DataType> ReplaceNext(LinkedPair<KeyType, DataType> newNext)
         {
             LinkedPair<KeyType, DataType> oldNext = next;
@@ -98,16 +137,29 @@ namespace KHHAHashMap
             return oldNext;
         }
 
+        /// <summary>
+        /// Replace next link
+        /// </summary>
+        /// <param name="newNext">New next pair</param>
         public void SetNext(LinkedPair<KeyType, DataType> newNext)
         {
             next = newNext;
         }
 
+        /// <summary>
+        /// Get the previous linked <see cref="LinkedPair{KeyType, DataType}"/>. Can be null
+        /// </summary>
+        /// <returns></returns>
         public LinkedPair<KeyType, DataType> GetPrevious()
         {
             return previous;
         }
 
+        /// <summary>
+        /// Replace previous link
+        /// </summary>
+        /// <param name="newPrevious">New previous pair</param>
+        /// <returns>Old previous pair</returns>
         public LinkedPair<KeyType, DataType> ReplacePrevious(LinkedPair<KeyType, DataType> newPrevious)
         {
             LinkedPair<KeyType, DataType> oldPrevious = previous;
@@ -115,11 +167,19 @@ namespace KHHAHashMap
             return oldPrevious;
         }
 
+        /// <summary>
+        /// Replace previous link
+        /// </summary>
+        /// <param name="newPrevious">New previous pair</param>
         public void SetPrevious(LinkedPair<KeyType, DataType> newPrevious)
         {
             previous = newPrevious;
         }
 
+        /// <summary>
+        /// Gets the last pair in the linked list
+        /// </summary>
+        /// <returns></returns>
         public LinkedPair<KeyType, DataType> GetLastLinkedPairInLink()
         {
             LinkedPair<KeyType, DataType> current = this;
@@ -140,6 +200,10 @@ namespace KHHAHashMap
             return current;
         }
 
+        /// <summary>
+        /// Gets the first pair in the linked list
+        /// </summary>
+        /// <returns></returns>
         public LinkedPair<KeyType, DataType> GetFirstLinkedPairInList()
         {
             LinkedPair<KeyType, DataType> current = this;
@@ -160,6 +224,11 @@ namespace KHHAHashMap
             return current;
         }
 
+        /// <summary>
+        /// Searches for a <see cref="LinkedPair{KeyType, DataType}"/> with a specified key that is linked with this one
+        /// </summary>
+        /// <param name="key">Search key</param>
+        /// <returns>Matched pair. Can be null</returns>
         public LinkedPair<KeyType, DataType> GetLinkedPairWithKeyInList(KeyType key)
         {
             LinkedPair<KeyType, DataType> pair = GetLinkedPairWithKeyInListForwardSearch(key);
@@ -167,6 +236,11 @@ namespace KHHAHashMap
             return pair;
         }
 
+        /// <summary>
+        /// Searches for a <see cref="LinkedPair{KeyType, DataType}"/> with a specified key that is linked with this one, only searching forwards in the list
+        /// </summary>
+        /// <param name="key">Search key</param>
+        /// <returns>Mathed pair. Can be null</returns>
         public LinkedPair<KeyType, DataType> GetLinkedPairWithKeyInListForwardSearch(KeyType key)
         {
             if (this.key.Equals(key)) return this;
@@ -179,6 +253,11 @@ namespace KHHAHashMap
             return null;
         }
 
+        /// <summary>
+        /// Search for a <see cref="LinkedPair{KeyType, DataType}"/> with a specified key that is linked with this one, only searching backwards in the list
+        /// </summary>
+        /// <param name="key">Search key</param>
+        /// <returns>Matched pair. Can be null</returns>
         public LinkedPair<KeyType, DataType> GetLinkedPairWithKeyInListBackwardSearch(KeyType key)
         {
             if (this.key.Equals(key)) return this;
@@ -191,30 +270,59 @@ namespace KHHAHashMap
             return null;
         }
 
+        /// <summary>
+        /// Determines whether or not the <see cref="DataType"/> is using the <see cref="IComparable"/> interface.
+        /// </summary>
         private void DetermineDataComparable()
         {
-            
+            isDataComparable = typeof(IComparable).IsAssignableFrom(typeof(DataType));
         }
 
+        /// <summary>
+        /// Part of the <see cref="IComparable"/> interface. Fails if the <see cref="DataType"/> doesn't implement it.
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
         public int CompareTo(object o)
         {
+            if (!isDataComparable) throw new NotSupportedException("Given data type is not comparable");
             return CompareTo((LinkedPair<KeyType, DataType>)o);
         }
 
+        /// <summary>
+        /// Wrapper for <see cref="CompareTo(object)"/>. Same fail condition
+        /// </summary>
+        /// <param name="pair"></param>
+        /// <returns></returns>
         public int CompareTo(LinkedPair<KeyType, DataType> pair)
         {
             if (!isDataComparable) throw new NotSupportedException("Given data type is not comparable");
             return ((IComparable)GetData()).CompareTo(pair);
         }
 
+        /// <summary>
+        /// Get an enumerator for the list. Part of the <see cref="IEnumerable"/> interface.
+        /// </summary>
+        /// <returns></returns>
         public IEnumerator GetEnumerator()
         {
             return new LinkedPairEnumerator(this);
         }
 
+        /// <summary>
+        /// Enumerator class for the list.
+        /// 
+        /// This class's sole purpose is to iterate through the <see cref="LinkedPair{KeyType, DataType}"/> list. It is self-contained and does not write anything to the <see cref="LinkedPair{KeyType, DataType}"/>.
+        /// </summary>
         private class LinkedPairEnumerator : IEnumerator
         {
+            /// <summary>
+            /// First pair in list
+            /// </summary>
             public LinkedPair<KeyType, DataType> firstPairInList;
+            /// <summary>
+            /// Current pair in iteration of the list
+            /// </summary>
             public LinkedPair<KeyType, DataType> currentPairInList;
 
             public object Current { get => currentPairInList; }
@@ -225,11 +333,18 @@ namespace KHHAHashMap
                 currentPairInList = firstPairInList;
             }
 
+            /// <summary>
+            /// Gets next pair in list. Part of <see cref="IEnumerator"/> interface
+            /// </summary>
+            /// <returns></returns>
             public bool MoveNext()
             {
                 return currentPairInList.GetNext() != null;
             }
 
+            /// <summary>
+            /// Part of <see cref="IEnumerator"/> interface
+            /// </summary>
             public void Reset()
             {
                 currentPairInList = firstPairInList;
